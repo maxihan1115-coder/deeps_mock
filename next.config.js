8f11b138135def11251442052ad9b7c81085641d
maxihan1115-coder <maxihan1115-coder@github.com>
2025-09-09 15:44:00 +0900

    next config 수정

--- next.config.js
+++ next.config.js
@@ -13,6 +13,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  
+  // EC2에서만 타입체크 스킵
+  typescript: {
+    ignoreBuildErrors: process.env.SKIP_TYPE_CHECK === 'true',
+  },
+  
+  eslint: {
+    ignoreDuringBuilds: process.env.SKIP_TYPE_CHECK === 'true',
+  },
 }
 
 module.exports = nextConfig
